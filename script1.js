13e2f3744daa0935f82194e4d582f615e9387ef9
DerGecko92 <dergecko92@gmail.com>
2024-05-29 21:11:03 +0200

    Update and rename script.js to script1.js

--- script1.js
+++ script1.js
@@ -7,11 +7,9 @@ function checkSelection() {
 		}
 	});
 	if (selected.includes(2) && selected.includes(4) && selected.includes(6) && selected.includes(8) && selected.length === 4) {
-		document.getElementById("message").innerText = "ckihogryo";
-		document.getElementById("message").style.display = "block";
+		window.location.href = 'https://dergecko92.github.io/secretshrine.de/key1';
 	} else if (selected.includes(1) && selected.includes(3) && selected.includes(5) && selected.includes(7) && selected.includes(9) && selected.length === 5) {
-		document.getElementById("message").innerText = "Try Vigenere";
-		document.getElementById("message").style.display = "block";
+		window.location.href = 'https://dergecko92.github.io/secretshrine.de/hint1';
 	} else {
 		document.getElementById("message").style.display = "none";
 	}
